40294ac52111822f1c3b66d7157851c1ed885220
Jeroen <jeroenh13@live.nl>
2015-11-23 12:39:52 +0100

    first

---
+++ SE42_Opdracht1/src/test/java/tests/Opdracht1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import bank.domain.Account;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jeroen Hendriks
+ */
+public class Opdracht1 {
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
+    public EntityManager em;
+    
+    public Opdracht1() {
+        em = emf.createEntityManager();
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAccount()
+    {
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        assertNull(account.getId());
+        em.getTransaction().commit();
+        System.out.println("AccountId: " + account.getId());
+        //TODO: verklaar en pas eventueel aan
+        assertTrue(account.getId() > 0L);
+    }
+    
+
+}
