721eb4a138fa238d5aa3bfdcc36a0f5579144cc5
Jeroen <jeroenh13@live.nl>
2015-11-25 11:19:54 +0100

    connectionwurkt

--- SE42_Opdracht1/src/test/java/tests/Opdracht1.java
+++ SE42_Opdracht1/src/test/java/tests/Opdracht1.java
@@ -6,6 +6,9 @@
 package tests;
 
 import bank.domain.Account;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,6 +18,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import util.DatabaseCleaner;
 
 /**
  *
@@ -22,30 +26,17 @@ import static org.junit.Assert.*;
  */
 public class Opdracht1 {
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BankPU");
-    public EntityManager em;
+    public EntityManager em = emf.createEntityManager();
+    DatabaseCleaner dc = new DatabaseCleaner(em);
     
     public Opdracht1() {
-        em = emf.createEntityManager();
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
     }
 
+    /*
+    
+    */
     @Test
-    public void testAccount()
+    public void testAccount1()
     {
         Account account = new Account(111L);
         em.getTransaction().begin();
@@ -58,5 +49,90 @@ public class Opdracht1 {
         assertTrue(account.getId() > 0L);
     }
     
+    
+    @Test
+    public void testAccount2()
+    {
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        assertNull(account.getId());
+        em.getTransaction().rollback();
+        // TODO code om te testen dat table account geen records bevat. Hint: bestudeer/gebruik AccountDAOJPAImpl
+    }
+    
+    @Test
+    public void testAccount3()
+    {
+        Long expected = -100L;
+        Account account = new Account(111L);
+        account.setId(expected);
+        em.getTransaction().begin();
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        //assertNotEquals(expected, account.getId();
+        em.flush();
+        //TODO: verklaar en pas eventueel aan
+        //assertEquals(expected, account.getId();
+        em.getTransaction().commit();
+        //TODO: verklaar en pas eventueel aan
+    }
+    
+    
+     @Test
+    public void testAccount4()
+    {
+        Long expectedBalance = 400L;
+        Account account = new Account(114L);
+        em.getTransaction().begin();
+        em.persist(account);
+        account.setBalance(expectedBalance);
+        em.getTransaction().commit();
+        assertEquals(expectedBalance, account.getBalance());
+        //TODO: verklaar de waarde van account.getBalance
+        Long acId = account.getId();
+        account = null;
+        EntityManager em2 = emf.createEntityManager();
+        em2.getTransaction().begin();
+        Account found = em2.find(Account.class, acId);
+        //TODO: verklaar de waarde van found.getBalance
+        assertEquals(expectedBalance, found.getBalance());
+    }
+    
+    
+    @Test
+    public void testAccount5()
+    {
+        
+    
+    }
+    
+    @Test
+    public void testAccount6()
+    {
+        
+    
+    }
+    
+    @Test
+    public void testAccount7()
+    {
+        
+    
+    }
+    
+    @Test
+    public void testAccount8()
+    {
+        
+    
+    }
+    
+    @Test
+    public void testAccount9()
+    {
+        
+    
+    }
 
 }
