08a566c91ddb8dc21d81013147338c601a881077
Jeroen <jeroenh13@live.nl>
2015-12-01 14:50:44 +0100

    jpimpl

--- SE42 auction/auction/src/main/java/auction/dao/UserDAOJPAImpl.java
+++ SE42 auction/auction/src/main/java/auction/dao/UserDAOJPAImpl.java
@@ -1,53 +1,52 @@
 package auction.dao;
 
 import auction.domain.User;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
 
 public class UserDAOJPAImpl implements UserDAO {
 
-    private HashMap<String, User> users;
-
-    public UserDAOJPAImpl() {
-        users = new HashMap<String, User>();
+    private final EntityManager em;
+    
+    public UserDAOJPAImpl(EntityManager em) {
+        this.em = em;
     }
 
     @Override
     public int count() {
-        return users.size();
+        Query q = em.createNamedQuery("User.count",User.class);
+        return ((Long) q.getSingleResult()).intValue();
     }
 
     @Override
     public void create(User user) {
-         if (findByEmail(user.getEmail()) != null) {
-            throw new EntityExistsException();
-        }
-        users.put(user.getEmail(), user);
+        em.persist(user);
     }
 
     @Override
     public void edit(User user) {
-        if (findByEmail(user.getEmail()) == null) {
-            throw new IllegalArgumentException();
-        }
-        users.put(user.getEmail(), user);
+        em.merge(user);
     }
 
 
     @Override
     public List<User> findAll() {
-        return new ArrayList<User>(users.values());
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(User.class));
+        return em.createQuery(cq).getResultList();
     }
 
     @Override
     public User findByEmail(String email) {
-        return users.get(email);
+        Query q = em.createNamedQuery("User.findByEmail", User.class);
+        q.setParameter("email", email);
+        return (User) q.getSingleResult();
     }
 
     @Override
     public void remove(User user) {
-        users.remove(user.getEmail());
+        em.remove(em.merge(user));
     }
 }
