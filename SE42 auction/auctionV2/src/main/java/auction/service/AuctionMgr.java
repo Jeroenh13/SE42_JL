0aa62d07767f28d292e18c835b1534d0c24a1784
liespies5 <lisa@MacBook-Pro-van-Lisa.local>
2015-12-08 14:28:06 +0100

    auctionmanager aangepast

--- SE42 auction/auctionV2/src/main/java/auction/service/AuctionMgr.java
+++ SE42 auction/auctionV2/src/main/java/auction/service/AuctionMgr.java
@@ -1,4 +1,5 @@
 package auction.service;
+
 import auction.dao.ItemDAO;
 import auction.dao.ItemDAOJPAImpl;
 import auction.dao.UserDAO;
@@ -12,27 +13,25 @@ import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class AuctionMgr  {
-    
+public class AuctionMgr {
+
     private ItemDAO itemDAO;
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("auctionPU");
-    
-    public AuctionMgr()
-    {
+
+    public AuctionMgr() {
         itemDAO = new ItemDAOJPAImpl(emf.createEntityManager());
     }
-    
-   /**
+
+    /**
      * @param id
      * @return het item met deze id; als dit item niet bekend is wordt er null
-     *         geretourneerd
+     * geretourneerd
      */
     public Item getItem(Long id) {
         return itemDAO.find(id);
     }
 
-  
-   /**
+    /**
      * @param description
      * @return een lijst met items met @desciption. Eventueel lege lijst.
      */
@@ -45,10 +44,11 @@ public class AuctionMgr  {
      * @param buyer
      * @param amount
      * @return het nieuwe bod ter hoogte van amount op item door buyer, tenzij
-     *         amount niet hoger was dan het laatste bod, dan null
+     * amount niet hoger was dan het laatste bod, dan null
      */
     public Bid newBid(Item item, Account buyer, Money amount) {
-        // TODO 
-        return null;
+        Bid b = item.newBid(buyer, amount);
+        itemDAO.create(item);
+        return b;
     }
 }
