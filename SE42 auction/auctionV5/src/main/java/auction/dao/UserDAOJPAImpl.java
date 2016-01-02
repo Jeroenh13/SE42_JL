a47d8e3eb414b238db1f149c8228d9c1a9eab256
Jeroenh13 <jeroenh13@live.nl>
2016-01-02 11:56:16 +0100

    auctionv5

---
+++ SE42 auction/auctionV5/src/main/java/auction/dao/UserDAOJPAImpl.java
@@ -0,0 +1,53 @@
+package auction.dao;
+
+import auction.domain.Account;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.Query;
+import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaQuery;
+
+public class UserDAOJPAImpl implements UserDAO {
+
+    private final EntityManager em;
+
+    public UserDAOJPAImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public int count() {
+        Query q = em.createNamedQuery("Account.count", Account.class);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+    @Override
+    public void create(Account user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void edit(Account user) {
+        em.merge(user);
+    }
+
+    @Override
+    public List<Account> findAll() {
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Account.class));
+        return em.createQuery(cq).getResultList();
+    }
+
+    @Override
+    public Account findByEmail(String email) {
+        Query q = em.createNamedQuery("Account.findByEmail", Account.class);
+        q.setParameter("email", email);
+        return (Account) q.getSingleResult();
+    }
+
+    @Override
+    public void remove(Account user) {
+        em.remove(em.merge(user));
+    }
+}
